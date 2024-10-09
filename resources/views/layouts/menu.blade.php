55d3a7dd86f83d20e01788152053e4218c36227a
Seno Dwi Prasetyo <senothoha77@gmail.com>
2024-10-09 15:26:48 +0700

    Integrasi template adminlte

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,120 @@
+<!-- Sidebar Menu -->
+<nav class="mt-2">
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <!-- Add icons to the links using the .nav-icon class
+        with font-awesome or any other icon font library -->
+    
+    
+    
+    <li class="nav-header">EXAMPLES</li>
+    <li class="nav-item">
+    <a href="../calendar.html" class="nav-link active">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+        <p>
+        Dashboard
+        {{--  <span class="badge badge-info right">2</span>  --}}
+        </p>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a href="../gallery.html" class="nav-link">
+        <i class="nav-icon far fa-image"></i>
+        <p>
+        Gallery
+        </p>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a href="../kanban.html" class="nav-link">
+        <i class="nav-icon fas fa-columns"></i>
+        <p>
+        Kanban Board
+        </p>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon far fa-envelope"></i>
+        <p>
+        Mailbox
+        <i class="fas fa-angle-left right"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+        <li class="nav-item">
+        <a href="../mailbox/mailbox.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Inbox</p>
+        </a>
+        </li>
+        <li class="nav-item">
+        <a href="../mailbox/compose.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Compose</p>
+        </a>
+        </li>
+        <li class="nav-item">
+        <a href="../mailbox/read-mail.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Read</p>
+        </a>
+        </li>
+    </ul>
+    </li>
+    <li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-book"></i>
+        <p>
+        Pages
+        <i class="fas fa-angle-left right"></i>
+        </p>
+    </a>
+    
+    </li>
+    <li class="nav-item menu-open">
+    <a href="#" class="nav-link active">
+        <i class="nav-icon far fa-plus-square"></i>
+        <p>
+        Extras
+        <i class="fas fa-angle-left right"></i>
+        </p>
+    </a>
+    
+    </li>
+    <li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-search"></i>
+        <p>
+        Search
+        <i class="fas fa-angle-left right"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+        <li class="nav-item">
+        <a href="../search/simple.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Simple Search</p>
+        </a>
+        </li>
+        <li class="nav-item">
+        <a href="../search/enhanced.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Enhanced</p>
+        </a>
+        </li>
+    </ul>
+    </li>
+    <li class="nav-header">EXIT</li>
+    
+    <li class="nav-item">
+    <a href="{{url('logout')}}" class="nav-link">
+        <i class="nav-icon fa fa-power-off"></i>
+        <p>Logout</p>
+    </a>
+    </li>
+    
+    
+    
+</ul>
+</nav>
+<!-- /.sidebar-menu -->
--- resources/views/operator/beranda.blade.php
+++ resources/views/operator/beranda.blade.php
@@ -1,23 +1,39 @@
-@extends('layouts.app')
+@extends('layouts.app_adminlte')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Welcome, Operator</div>
+<section class="content">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <!-- Default box -->
+    <div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Welcome, Operator</h3>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+        <div class="card-tools">
+        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-minus"></i>
+        </button>
+        <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+        </button>
         </div>
     </div>
-</div>
+    <div class="card-body">
+        
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        {{ __('You are logged in!') }}
+    </div>
+    <!-- /.card-body -->
+    {{--  <div class="card-footer">
+        Footer
+    </div>  --}}
+    <!-- /.card-footer-->
+    </div>
+    <!-- /.card -->
+
+</section>
 @endsection
